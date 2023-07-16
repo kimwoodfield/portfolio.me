411f7da2640ecbf1d1bcb1f5f5a186d92f11673d
kimwoodfield <kimwoodfield@live.com>
2023-07-16 20:10:29 +1000

    feat: add some styles for dynamic blog pages

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const Page = ({ children }: Props) => {
     return (
         <div className='p-6'>
-            <div className='m-0 sm:md-12 md:mt-20 lg:mt-40 flex justify-center'>
+            <div className='m-0 sm:md-12 md:mt-20 flex justify-center'>
                 <div className='max-w-[1080px] flex flex-col gap-20'>
                     {children}
                 </div>
